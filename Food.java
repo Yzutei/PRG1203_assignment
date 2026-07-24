d9a5ce3490c1a5b1c907c77393d94fa193289d86
Yzutei <138152764+Yzutei@users.noreply.github.com>
2026-07-24 15:53:55 +0800

    add: Food.java thingy

---
+++ Food.java
@@ -0,0 +1,15 @@
+public class Food extends Item {
+    public Food() {
+        super();
+    }
+
+    public Food(String name, double price, String description, String category) {
+        super(name, price, description, category);
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Food details");
+        super.displayDetails();
+    }
+}
