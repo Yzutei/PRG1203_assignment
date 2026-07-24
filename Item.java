f88b507178e96ba77690c0ecf3d320facdf0f6b3
Yzutei <138152764+Yzutei@users.noreply.github.com>
2026-07-24 15:15:33 +0800

    add:init

---
+++ Item.java
@@ -0,0 +1,10 @@
+public abstract class Item {
+    private String name;
+    private double price;
+    private String description;
+    private String category;
+}
+
+    public Item(){
+        
+    }
